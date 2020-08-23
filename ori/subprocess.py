87f9137f92390d8430764b6e261c582e9ebc258c
James Mishra <james@neocrym.com>
2020-08-23 05:55:17 -0500

    Make sure that our subprocess code can pickle the functions we pass it

--- ori/subprocess.py
+++ ori/subprocess.py
@@ -1,5 +1,6 @@
 """Utility function for running external commands as subprocesses."""
 import multiprocessing
+import os
 import selectors
 import signal
 import subprocess
@@ -15,9 +16,59 @@ def _null_function(*args, **kwargs):  # pylint: disable=unused-argument
     """
 
 
+def _target(
+    *,
+    command: typing.Sequence[typing.Union[bytes, str, os.PathLike]],
+    stdout_function: typing.Optional[typing.Callable] = None,
+    stderr_function: typing.Optional[typing.Callable] = None,
+    exception_function: typing.Optional[typing.Callable] = None,
+    exit_code_on_killed: int = 0,
+):
+    """Manage the actual command we're running."""
+    if not stdout_function:
+        stdout_function = _null_function
+    if not stderr_function:
+        stderr_function = _null_function
+    if not exception_function:
+        exception_function = _null_function
+    command_process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True,
+    )
+
+    def kill_handler(signum, frame):  # pylint: disable=unused-argument
+        """
+        If the control process receives a signal to stop, then make sure to stop the command process."""
+        command_process.terminate()
+        sys.exit(exit_code_on_killed)
+
+    try:
+        signal.signal(signal.SIGHUP, kill_handler)
+        signal.signal(signal.SIGTERM, kill_handler)
+        signal.signal(signal.SIGINT, kill_handler)
+
+        # Listen on stdout and stderr in parallel, preserving order,
+        # writing stdout to debug logs and stderr to info logs.
+        sel = selectors.DefaultSelector()
+        sel.register(command_process.stdout, selectors.EVENT_READ)
+        sel.register(command_process.stderr, selectors.EVENT_READ)
+        while True:
+            for key, _ in sel.select():
+                data = key.fileobj.read1().decode()
+                if not data:
+                    return
+                if key.fileobj is command_process.stdout:
+                    stdout_function(data)
+                else:
+                    stderr_function(data)
+    except BaseException as exc:
+        exception_function(exc)
+        command_process.terminate()
+        raise
+
+
 def run_process_in_background(
     *,
-    command: typing.Iterable[str],
+    command: typing.Sequence[typing.Union[bytes, str, os.PathLike]],
     stdout_function: typing.Optional[typing.Callable] = None,
     stderr_function: typing.Optional[typing.Callable] = None,
     exception_function: typing.Optional[typing.Callable] = None,
@@ -27,6 +78,7 @@ def run_process_in_background(
     Runs `command`, writing stdout and stderr to the logger in `logger`.
 
     This function is based on `this answer on StackOverflow <https://stackoverflow.com/questions/31833897/python-read-from-subprocess-stdout-and-stderr-separately-while-preserving-order/56918582#56918582>`_.
+
 
     Args:
         command: This is an iterable--like a list--of strings. This iterable
@@ -49,50 +101,30 @@ def run_process_in_background(
     Returns:
         This returns an already-started :class:`multiprocessing.Process` instance,
         which you can use to monitor or kill the process as time goes on.
+
+    Warning:
+        The parameters **stdout_function**, **stderr_function** and **exception_function**
+        are used to log the standard output, standard error, and any exceptions that
+        your command might raise. These functions are run in a Python subprocess,
+        which means that they must be *importable* at the module level and
+        *pickleable*. Closures, which are functions enclosed in other functions,
+        are not pickleable.
+
+        If you feel like you need to add additional parameters or state to your
+        logging functions, you can write your functions as instances of
+        Python classes with the
+        `__call__() magic method <https://www.geeksforgeeks.org/__call__-in-python/>`_.
+
     """
-
-    if not stdout_function:
-        stdout_function = _null_function
-    if not stderr_function:
-        stderr_function = _null_function
-    if not exception_function:
-        exception_function = _null_function
-
-    def target():
-        """Manage the actual command we're running."""
-        command_process = subprocess.Popen(
-            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True,
-        )
-        try:
-            # If the control process receives a signal to stop,
-            # then make sure to stop the command process.
-            def kill_handler(signum, frame):  # pylint: disable=unused-argument
-                command_process.terminate()
-                sys.exit(exit_code_on_killed)
-
-            signal.signal(signal.SIGHUP, kill_handler)
-            signal.signal(signal.SIGTERM, kill_handler)
-            signal.signal(signal.SIGINT, kill_handler)
-
-            # Listen on stdout and stderr in parallel, preserving order,
-            # writing stdout to debug logs and stderr to info logs.
-            sel = selectors.DefaultSelector()
-            sel.register(command_process.stdout, selectors.EVENT_READ)
-            sel.register(command_process.stderr, selectors.EVENT_READ)
-            while True:
-                for key, _ in sel.select():
-                    data = key.fileobj.read1().decode()
-                    if not data:
-                        return
-                    if key.fileobj is command_process.stdout:
-                        stdout_function(data)
-                    else:
-                        stderr_function(data)
-        except BaseException as exc:
-            exception_function(exc)
-            command_process.terminate()
-            raise
-
-    control_process = multiprocessing.Process(target=target)
+    control_process = multiprocessing.Process(
+        target=_target,
+        kwargs=dict(
+            command=command,
+            stdout_function=stdout_function,
+            stderr_function=stderr_function,
+            exception_function=exception_function,
+            exit_code_on_killed=exit_code_on_killed,
+        ),
+    )
     control_process.start()
     return control_process
--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -1,59 +1,40 @@
 """Unit test module for :mod:`ori.subprocess`."""
-import contextlib
 import os
+import tempfile
 import unittest
 import uuid
 
 import ori.subprocess
 
 
-@contextlib.contextmanager
-def get_temporary_filename():
-    """Context manager to open a temporary file in the current directory and delete it afterwards.
+class LogToFile:
+    """Create a function that continually appends to a file."""
 
-    This returns the filename as a string. The user has to reopen it to get a
-    file handle.
+    def __init__(self, filename: str):
+        self.filename = filename
 
-    This is needed because temporary files created with Python's
-    `tempfile.NamedTemporaryFile` cannot be reopened under Windows NT or later.
-    """
-    filename = str(uuid.uuid4()) + ".txt"
-    open(filename, "w").close()
-    try:
-        yield filename
-    finally:
-        try:
-            os.remove(filename)
-        except FileNotFoundError:
-            pass
-
-
-def create_file_logger(filename):
-    """Creates a simple file logger that can be used in the background."""
-
-    def printer(value):
-        """Append the value to the enclosed filename."""
-        with open(filename, "a") as fh:
-            return fh.write(value)
-
-    return printer
+    def __call__(self, message: str):
+        with open(self.filename, "a") as handle:
+            return handle.write(message)
 
 
 class TestSubprocess(unittest.TestCase):
     """Unit tests for ori.subprocess."""
 
     def test__run_process_in_background__stdout(self):
-        """Check that we can log stdout after running a function in the background."""
-        with get_temporary_filename() as log_filename:
-            logger = create_file_logger(log_filename)
-            if os.name == "nt":
-                command = "dir"
-            else:
-                command = "ls"
-            process = ori.subprocess.run_process_in_background(
-                command=[command, log_filename], stdout_function=logger,
-            )
-            process.join()
-            with open(log_filename, "r") as fh:
-                output = fh.read()
-            assert log_filename in output
+        """Test that we can run a command in the background while logging stdout."""
+        temp_dir = tempfile.TemporaryDirectory()
+        temp_filename = os.path.join(temp_dir.name, str(uuid.uuid4()) + ".txt")
+        log_to_file = LogToFile(temp_filename)
+        if os.name == "nt":
+            command = "dir"
+        else:
+            command = "ls"
+        process = ori.subprocess.run_process_in_background(
+            command=[command, __file__], stdout_function=log_to_file,
+        )
+        process.join()
+        with open(temp_filename, "r") as fh:
+            output = fh.read()
+        temp_dir.cleanup()
+        assert "test_subprocess.py" in output
